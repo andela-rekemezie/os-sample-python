03c4e94dfaaf5a2ba3fc41a07a7140b1351c804a
andela-rekemezie <rowland.ekemezie@andela.com>
2016-08-05 17:25:19 +0100

    Add updated name greeting

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World Rowland!"
+    return "Hello World Rowland Ekemezie!"
 
 if __name__ == "__main__":
     application.run()
