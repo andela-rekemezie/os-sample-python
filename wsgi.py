ae9a982f83e9152f9474e92881bcff9c9f3cc0bc
andela-rekemezie <rowland.ekemezie@andela.com>
2016-08-05 17:27:48 +0100

    Add login route

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,11 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World Rowland Ekemezie!"
+    return "hell man!"
+
+@application.route("/login")
+def login():
+		return "This is the Loging page. Josh, please login"
 
 if __name__ == "__main__":
     application.run()
